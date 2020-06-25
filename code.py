536b3f2493ce295b8ed734efa4afd1d7f49cbb2a
dragosolomon <dragos07solomon@gmail.com>
2020-06-25 13:03:21 +0300

    decoded the words from bytes into string types

--- code.py
+++ code.py
@@ -3,7 +3,7 @@ from urllib.request import urlopen
 story = urlopen("hhtp://sixty-north.com/c/t.txt")
 words = []
 for line in story:
-    line_words = line.split()
+    line_words = line.decode('utf8').split()
     for word in line_words:
         words.append(word)
 
