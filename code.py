e85262523a62007480fbb690097ebd2e025246b2
dragosolomon <dragos07solomon@gmail.com>
2020-06-28 13:46:44 +0300

    made a funcion fetch_words witch fetches all the words from tthe story

--- code.py
+++ code.py
@@ -1,12 +1,13 @@
 from urllib.request import urlopen
 
-story = urlopen("http://sixty-north.com/c/t.txt")
-words = []
-for line in story:
-    line_words = line.decode('utf8').split()
-    for word in line_words:
-        words.append(word)
+def fetch_words():
+    story = urlopen("http://sixty-north.com/c/t.txt")
+    words = []
+    for line in story:
+        line_words = line.decode('utf8').split()
+        for word in line_words:
+            words.append(word)
+    story.close()
 
-story.close()
 for word in words:
     print(word)
