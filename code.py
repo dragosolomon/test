85a7397812195eaa2a423ade29e6441534f2f44f
dragosolomon <dragos07solomon@gmail.com>
2020-06-28 14:37:14 +0300

    added(fixed) print loop at the end

--- code.py
+++ code.py
@@ -2,12 +2,14 @@ from urllib.request import urlopen
 
 def fetch_words():
     story = urlopen("http://sixty-north.com/c/t.txt")
+
     words = []
+
     for line in story:
         line_words = line.decode('utf8').split()
         for word in line_words:
             words.append(word)
     story.close()
 
-for word in words:
-    print(word)
+    for word in words:
+        print(word)
