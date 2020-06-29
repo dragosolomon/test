ed07621977784a72793f9ca2b8810a213d4842b9
dragosolomon <dragos07solomon@gmail.com>
2020-06-29 12:27:59 +0300

    added funcstions print_words(story_words) and main()

--- code.py
+++ code.py
@@ -9,10 +9,17 @@ def fetch_words():
         line_words = line.decode('utf8').split()
         for word in line_words:
             story_words.append(word)
-    story.close()
 
+    story.close()
+    return story_words
+
+def print_words(story_words)
     for word in story_words:
         print(word)
 
+def main():
+    words = fetch_words()
+    print_words(words)
+    
 if __name__ == '__main__':
     fetch_words()
